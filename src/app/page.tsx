dc526b7af127885af7714f351310a58e3632175c
Ankit <ankitojha1409@gmail.com>
2024-06-12 11:43:53 +0530

    imported gemini API and tested it on postman to get correct response

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,54 @@
 "use client";
 
+import { log } from "console";
 import { useState } from "react";
+
+const API_KEY = process.env.NEXT_PUBLIC_GEMINI_API;
 
 export default function Home() {
   const [text, setText] = useState("");
+  const [fixedText, setFixedText] = useState("");
+
+  const trainingPrompt = [
+    {
+      parts: [
+        {
+          text: "From next prompt I will be just providing you some text or sentences or paragraphs. Rewrite the grammar for me",
+        },
+      ],
+      role: "user",
+    },
+    {
+      parts: [
+        {
+          text: "Okay",
+        },
+      ],
+      role: "model",
+    },
+    {
+      parts: [
+        {
+          text: "Wut iz my nayme",
+        },
+      ],
+      role: "user",
+    },
+  ];
+  const fixText = async () => {
+    console.log(text);
+  };
 
   return (
     <div className="w-full h-screen flex items-center justify-around flex-col sm:flex-row overflow-auto gap-10 p-8">
       <div className="flex justify-center w-full sm:w-1/2 border-2 rounded-3xl min-h-[64vh] max-h-[80vh] overflow-hidden">
-        <p className="text-md font-normal p-5 overflow-auto">
-          This is home page Lorem ipsum dolor sit amet consectetur, adipisicing
-          elit. Doloremque laboriosam voluptatum accusamus. Blanditiis hic
-          quisquam maiores obcaecati. Porro in ullam recusandae illo dicta,
-          nostrum incidunt corrupti animi. Quam repellat ipsum eius minima
-          voluptatibus obcaecati doloremque earum, modi magnam officia sequi in
-          aliquid quidem fuga eos unde, doloribus facere pariatur officiis
-          molestiae corporis totam sed atque! Illo provident qui, eos
-          consequatur necessitatibus maxime a officiis natus quisquam, ratione
-          commodi mollitia. Voluptatibus iure quas delectus dicta, blanditiis
-          incidunt eius ab deleniti, qui necessitatibus eaque aliquid magnam
-          libero labore! Repellendus, assumenda iure saepe laudantium facilis.
-          nostrum incidunt corrupti animi. Quam repellat ipsum eius minima
-          voluptatibus obcaecati doloremque earum, modi magnam officia sequi in
-          aliquid quidem fuga eos unde, doloribus facere pariatur officiis
-          molestiae corporis totam sed atque! Illo provident qui, eos
-          consequatur necessitatibus maxime a officiis natus quisquam, ratione
-          commodi mollitia. Voluptatibus iure quas delectus dicta, blanditiis
-          incidunt eius ab deleniti, qui necessitatibus eaque aliquid magnam
-          libero labore! Repellendus, assumenda iure saepe laudantium facilis.
-        </p>
+        {fixedText.length > 0 ? (
+          <p className="text-md font-normal p-5 overflow-auto"></p>
+        ) : (
+          <p className="text-md font-normal p-5 overflow-auto">
+            prompt is empty...
+          </p>
+        )}
       </div>
       <div className="flex items-start justify-start flex-col w-full sm:w-1/2 border-2 rounded-3xl max-h-[80vh] p-3 overflow-hidden">
         <textarea
@@ -39,7 +59,11 @@ export default function Home() {
             setText(e.target.value);
           }}
         />
-        <button className="w-full mt-4 p-4 bg-yellow-400 text-white text-lg font-semibold rounded-full">
+
+        <button
+          className="w-full mt-4 p-4 bg-yellow-400 text-white text-lg font-semibold rounded-full"
+          onClick={fixText}
+        >
           Fix
         </button>
       </div>
